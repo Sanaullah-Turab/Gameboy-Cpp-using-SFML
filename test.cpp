a641f361a3d705b7e94a6308333c8eecce638fef
Sanaullah-Turab <sanaullahmalik2003@gmail.com>
2024-12-11 15:34:12 +0500

    Remove test file

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
-    std::cout << "SFML works!" << std::endl;
-    return 0;
-}
